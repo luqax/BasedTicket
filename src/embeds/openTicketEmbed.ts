88c3fe350882dfcb9dc44db42f9bff5c482efa94
Mixeu <106706845+Mixeu02@users.noreply.github.com>
2023-10-23 00:55:34 -0300

    Made the basic boilerplate for the index.ts file

---
+++ src/embeds/openTicketEmbed.ts
@@ -0,0 +1,8 @@
+import { EmbedBuilder } from "@discordjs/builders"
+
+export { 
+    new EmbedBuilder()
+        .setTitle("Tickets")
+        .setDescription("Abra um ticket"),
+    "e"
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client: Client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages
+    ]
+});
